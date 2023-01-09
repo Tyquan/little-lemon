e71477dac0f699bbb920415afe0315cfcec932c9
Tyquan Reddick <Tyquanjreddick@gmail.com>
2023-01-09 12:20:19 -0500

    nav style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,22 @@
-import logo from '../logo.png';
+import logo from '../images/logo.png';
+import hamburgerIcon from '../images/handburgerIcon.png';
+import cart from '../images/cart.jpg';
 import Nav from './Nav';
 
 const Header = () => {
     return (
         <header>
+            <div id="navButton">
+                <img src={hamburgerIcon} id="navImage" alt="Nav Button" />
+            </div>
             <div id="logoComponent">
                 <img id="logoImage" src={logo} alt="Little Lemon Logo" />
             </div>
             <div id="navComponent">
                 <Nav />
+            </div>
+            <div id="cartButton">
+                <img src={cart} id="cartImage" alt="Cart Button" />
             </div>
         </header>
     );
